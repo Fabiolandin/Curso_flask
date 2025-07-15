4346993e190c8003020fcbfb8947b8179dde4b76
Fabio Landin <flandin1990@gmail.com>
2025-07-15 16:08:00 -0300

    Definindo rota dos clientes

--- main.py
+++ main.py
@@ -1,28 +1,11 @@
 #impor do flask
-from flask import Flask, url_for, render_template
+from flask import Flask
+from routes.home import home_route
 
 #Inicializando Flask
 app = Flask(__name__)
 
-#Rotas
-@app.route("/")
-def ola_mundo():
-    titulo = "Gestão de Usuários"
-    usuarios = [
-        {"nome": "Fabio", "membro_ativo": True},
-        {"nome": "Flavia", "membro_ativo": False},
-        {"nome": "Angelina", "membro_ativo": True},
-    ]
-    return render_template("index.html", titulo = titulo, usuarios = usuarios)
-
-#Rotas
-@app.route("/sobre")
-def pagina_sobre():
-    return """
-    Testando docstrings
-    1, 2, 3, 4, 5, 6, 7, 8, 9
-    10!
-"""
+app.register_blueprint(home_route)
 
 #Executando Flask
 app.run(debug=True)
